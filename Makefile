345533205dd7a437f38b631742117292b4752c23
Daniel <dawg@pop-os.localdomain>
2020-12-17 12:16:15 +0100

    after feedback

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build: src/Main.elm
 	elm make src/Main.elm
 
 run: src/Main.elm
-    elm-live src/Main.elm
+	elm-live src/Main.elm
--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Css exposing (Pct, pct)
+import Css exposing (Pct, paddingTop, pct)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
@@ -56,15 +56,15 @@ scaled =
 
 
 normalFontSize =
-    Font.size (scaled 1)
+    Font.size 20
 
 
 mediumFontSize =
-    Font.size (scaled 2)
+    Font.size 40
 
 
 largeFontSize =
-    Font.size (scaled 3)
+    Font.size 64
 
 
 
@@ -108,15 +108,15 @@ view model =
     layout [ inFront header, width fill, height fill ] <|
         column [ locationId Front, width fill ]
             -- set the location id to front here rather than in the "front" function to ensure that we jump all the way to the top of the page
-            [ header, front, quote, forening, footer ]
+            [ header, front, quote, forening, events, supportUs, contact, footer ]
+
+
+blueLogo =
+    img [ width <| px 350, paddingEach { top = 25, bottom = 25, left = 0, right = 0 } ] "logo_blue.png"
 
 
 
 -- extremely ugly way to create a box - is there a better way?
-
-
-blueLogo =
-    img [ width <| px 350, zIndex 1000 ] "logo_blue.png"
 
 
 box props =
@@ -128,9 +128,59 @@ boxBehind opts =
 
 
 front =
-    el [ paddingEach { top = 55, bottom = 40, left = 0, right = 0 }, width fill, boxBehind [ Background.color orange, moveLeft 128, height <| px 550 ] ] <|
-        column [ centerX, width fill ]
-            [ row [ centerX ] [ column [ moveUp 45, spacing 16 ] [ blueLogo, img [ width <| px 400 ] "gertrud.png" ], el [ moveLeft 20 ] about ]
+    el
+        [ paddingEach { top = 50, bottom = 40, left = 0, right = 0 }
+        , width fill
+        , centerX
+        , boxBehind [ Background.color orange, moveLeft 128, height <| px 550 ]
+        ]
+    <|
+        row [ width fill, centerX ]
+            [ column [ moveUp 45, spacing 16, centerX ] [ blueLogo, img [ width <| px 400 ] "gertrud.png" ]
+            , about
+            ]
+
+
+supportUs =
+    el [ locationId SupportUs, width fill, height <| px 700, padding 64, boxBehind [ Background.color blue, height (px 550), moveLeft 100 ] ] <|
+        column [ width fill, centerX ]
+            [ heading white orange "STØT OS"
+            , row [ width fill, spacing 64 ]
+                [ multilineText
+                    [ spacing 16, width <| px 400, Font.color white ]
+                    [ "Vil du være medlem af PRALs forening?"
+                    , "Som medlem er du med til at støtte PRALs arbejde, så vi kan holde nogle anderledes, nørdede og fede events og blive ved med at være en forening, som bakker op om unges viden og giver dem rummet til at prale."
+                    , "Et støttemedlemskab koster 100 kr/året."
+                    , "Udfyld formularen nedenfor, så modtager du en mail fra os om, hvordan du indbetaler."
+                    ]
+                , img [ width <| px 500 ] "livfreja.jpg"
+                ]
+            ]
+
+
+events =
+    el [ locationId Events, width fill, height <| px 700, padding 64, boxBehind [ Background.color blue, height (px 550), moveLeft 100 ] ] <|
+        column [ width fill, centerX, spacing 64 ]
+            [ heading white orange "EVENTS"
+            , row [ width fill, spacing 64 ]
+                [ paragraph [ Font.color white, alignTop ] [ text "Klik ", pageLink "her" (locationToLink NextEvent) ]
+                , img [ width <| px 600 ] "greta.jpg"
+                ]
+            ]
+
+
+contact =
+    el [ locationId Contact, width fill, height <| px 700, padding 64, boxBehind [ Background.color orange, height (px 550) ] ] <|
+        column [ width fill, centerX ]
+            [ heading white blue "KONTAKT OS"
+            , row [ width fill, spacing 64 ]
+                [ img [] "fish.png"
+                , multilineText
+                    [ spacing 16, width <| px 400, Font.color white ]
+                    []
+
+                --, img [ width <| px 500 ] "livfreja.jpg"
+                ]
             ]
 
 
@@ -142,7 +192,6 @@ flamaFont =
             }
         , Font.sansSerif
         ]
-    , Font.bold
     ]
 
 
@@ -167,6 +216,7 @@ locationToStr loc =
             "nextevent"
 
 
+locationToLink : Location -> String
 locationToLink loc =
     case loc of
         NextEvent ->
@@ -176,20 +226,21 @@ locationToLink loc =
             "#" ++ locationToStr loc
 
 
-
--- header : Element Msg
-
-
 header : Element msg
 header =
-    row [ width fill, spacing xLarge, padding 16, Background.color white ]
-        [ headerButton "FORSIDE" Front
-        , headerButton "NÆSTE BEGIVENHED" NextEvent
-        , headerButton "BAGOM" Behind
+    row
+        [ width fill
+        , spacing xLarge
+        , paddingEach { right = 128, left = 0, top = 16, bottom = 16 }
+        , Background.color white
 
-        -- , headerButton "EVENTS" Events
-        , headerButton "KONTAKT" Contact
+        --, moveLeft (128 - 16) -- move left to align with orange box in the background
+        ]
+        [ headerButton "FORSIDE" Front
+        , headerButton "BAGOM" Behind
+        , headerButton "EVENTS" Events
         , headerButton "STØT OS" SupportUs
+        , headerButton "KONTAKT" Contact
         ]
 
 
@@ -208,17 +259,36 @@ headerButton txt loc =
                , normalFontSize
                , Border.widthEach
                     { top = 0
-                    , bottom = 2
+                    , bottom = 2 -- for the blue underline
                     , left = 0
                     , right = 0
                     }
                , Border.color white
                , Element.mouseOver [ Border.color blue ]
-
-               -- underline text here
                ]
         )
         { url = locationToLink loc, label = text txt }
+
+
+pageLink : String -> String -> Element msg
+pageLink txt page =
+    link
+        (flamaFont
+            ++ [ Font.italic
+               , Font.bold
+               , normalFontSize
+               , Border.widthEach
+                    { top = 0
+                    , bottom = 2 -- for the blue underline
+                    , left = 0
+                    , right = 0
+                    }
+               , Border.color white
+
+               --, Element.mouseOver [ Border.color blue ]
+               ]
+        )
+        { url = page, label = text txt }
 
 
 zIndex : Int -> Attribute msg
@@ -233,7 +303,7 @@ setId id =
 
 heading : Color -> Color -> String -> Element msg
 heading fontColor =
-    boldWithShadow [ Font.color fontColor, centerX, largeFontSize ] 2
+    boldWithShadow [ Font.color fontColor, alignLeft, largeFontSize ] 4
 
 
 shadow : Color -> Float -> Attr decorative msg
@@ -243,14 +313,7 @@ shadow color offset =
 
 boldWithShadow : List (Attribute msg) -> Float -> Color -> String -> Element msg
 boldWithShadow props offset shadowColor txt =
-    el
-        (flamaFont ++ [ Font.bold, Font.italic, shadow shadowColor offset ] ++ props)
-        (text txt)
-
-
-regularText : List (Attribute msg) -> String -> Element msg
-regularText opts txt =
-    paragraph (flamaFont ++ [ normalFontSize, alignLeft ] ++ opts) [ text txt ]
+    unwrappedText ([ Font.bold, Font.italic, shadow shadowColor offset ] ++ props) txt
 
 
 wrapQuote : String -> String
@@ -266,13 +329,31 @@ quote : Element msg
 quote =
     column [ width fill, height <| px 450 ] <|
         [ el [ centerX ] <|
-            column [ width <| px 650, spacing 16, behindContent hand ]
-                [ "Jeg synes, det er fedt at få et indblik i andres passioner, og det synes jeg giver noget mod til mig selv."
-                    |> wrapQuote
-                    |> regularText [ Font.bold, Font.color blue, largeFontSize, Font.center ]
-                , regularText [ Font.color blue, largeFontSize, Font.center, Font.bold ] "Katja, 28 år"
+            multilineText
+                [ width <| px 650
+                , spacing 32
+                , Font.bold
+                , Font.italic
+                , Font.color blue
+                , mediumFontSize
+                , Font.center
+                , behindContent hand
+                ]
+                [ wrapQuote <| "jeg synes, det er fedt at få et indblik i andres passioner, og det synes jeg giver noget mod til mig selv"
+                , "Katja, 28 år"
                 ]
         ]
+
+
+inlineText opts str =
+    paragraph (flamaFont ++ [ alignLeft ] ++ opts) [ text str ]
+
+
+multilineText opts strs =
+    column opts <|
+        List.map
+            (inlineText [ spacing 8 ])
+            strs
 
 
 about : Element msg
@@ -280,16 +361,17 @@ about =
     el
         [ Background.color blue
         , width <| px 500
-        , height <| px 540
-        , padding 64
+        , centerX
+        , moveLeft 20
+        , padding 50
         ]
         (column [ width fill, spacing 32 ]
-            [ heading white orange "HVAD PRALER DU AF?"
-            , regularText [ Font.color white, Font.alignLeft ]
-                "Måske ikke så meget, men du ved helt sikkert noget om aktier, warhammers, dragrace eller noget helt andet."
-            , regularText [ Font.color white, Font.alignLeft ]
-                "PRAL hylder unges vilde viden, til et gratis event hver måned. Her praler en ny ung løs om sin passion, som vi sammen skal nørde. Ingen er eksperter, men alle er nysgerrige!"
-            , row [ spacing 16, centerX ] [ roundedButton "NÆSTE EVENT", roundedButton "STØT OS" ]
+            [ heading white orange "HVAD PRALER\nDU AF?"
+            , multilineText [ Font.color white, Font.alignLeft, spacing 32 ]
+                [ "Måske ikke så meget, men du ved helt sikkert noget om aktier, warhammers, dragrace eller noget helt andet."
+                , "PRAL hylder unges vilde viden, til et gratis event hver måned. Her praler en ny ung løs om sin passion, som vi sammen skal nørde."
+                , "Ingen er eksperter, men alle er nysgerrige!"
+                ]
             ]
         )
 
@@ -299,35 +381,30 @@ bullet n =
 
 
 bulletPoint n txt =
-    row [ spacing 16 ] [ bullet n, regularText [ Font.color white, alignTop ] txt ]
+    row [ spacing 16 ] [ bullet n, inlineText [ Font.color white, alignTop ] txt ]
 
 
 bulletPoints =
     column [ spacing 32, width fill, alignTop ]
         [ bulletPoint 1 "PRAL hylder nørderi og fordybelse."
-        , bulletPoint 2 "Unge skal prale om deres vilde viden  - PRAL giver dem talerstolen."
+        , bulletPoint 2 "Unge skal prale om deres vilde viden - PRAL giver dem talerstolen."
         , bulletPoint 3 "Præmissen er blot, du har en passion, du vil dele med os andre."
         ]
 
 
 forening =
-    el [ width fill, padding 64, boxBehind [ Background.color orange, height (px 500) ] ] <|
+    el [ locationId Behind, width fill, padding 64, boxBehind [ Background.color orange, height (px 500) ] ] <|
         column [ width fill, centerX, spacing 64 ]
-            [ boldWithShadow [ Font.size 32, Font.color white ] 2 blue "EN FORENING AF UNGE"
-            , row [ width fill, spacing 16, centerX ]
+            [ heading white blue "EN FORENING AF UNGE"
+            , row [ width fill, spacing 64, centerX ]
                 [ bulletPoints
-                , img [ width <| px 380 ] "christian.png"
+                , img [ width <| px 400 ] "christian.png"
                 ]
             ]
 
 
-unwrappedText txt =
-    el (flamaFont ++ [ Font.color white, normalFontSize ]) (text txt)
-
-
-roundedButton : String -> Element msg
-roundedButton =
-    boldWithShadow [ Background.color orange, Border.rounded 50, padding 8, mediumFontSize, Font.color white ] 2 blue
+unwrappedText props txt =
+    el (flamaFont ++ props) (text txt)
 
 
 footerEl txt other =
@@ -336,23 +413,22 @@ footerEl txt other =
 
 
 footerAbout =
-    footerEl "OM OS" <| column [ width fill, spacing 8 ] [ unwrappedText "PRAL", unwrappedText "CVR: 41719028" ]
+    footerEl "OM OS" <| multilineText [ width fill, Font.color white, spacing 8 ] [ "PRAL", "CVR: 41719028" ]
 
 
 footerContact =
     footerEl "KONTAKT OS" <|
-        column [ spacing 8 ]
-            [ unwrappedText "pral@hotmail.com"
-            , unwrappedText "xx xx xx xx"
+        multilineText [ Font.color white, spacing 8 ]
+            [ "pral@hotmail.com"
+            , "xx xx xx xx"
             ]
 
 
 footerFollow =
     footerEl "FØLG OS" <|
-        row [ spacing 8 ]
-            [ el [ Background.color orange ] none
-            , el [ Background.color orange ] none
-            , el [ Background.color orange ] none
+        row [ spacing 10 ]
+            [ link [] { url = "https://www.facebook.com/PRALkbh", label = img [ width <| px 40 ] "facebookicon.png" }
+            , link [] { url = "https://www.instagram.com/pral_kbh/", label = img [ width <| px 40 ] "instaicon.png" }
             ]
 
 
